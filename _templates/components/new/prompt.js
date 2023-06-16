ec7b01ca9fae01da49140c805d1b64ba8869fea4
Phuc Phan Hong <phuc.phan@3forcom.com>
2023-06-16 17:19:30 +0700

    :sparkles: config rollup, hygen

--- _templates/components/new/prompt.js
+++ _templates/components/new/prompt.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
-    type: 'input',
-    name: 'name',
+    type: "input",
+    name: "name",
     message: `Input your component's name`,
   },
 ];
---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: "react-lib",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    external: [/\.css$/],
+    plugins: [dts.default()],
+  },
+];
